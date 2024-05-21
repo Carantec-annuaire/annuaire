3b8d10a1f10df550b843d40fc975954c96cbbe97
Liesje123 <elisabeth.guitton@isen-ouest.yncrea.fr>
2024-05-21 10:43:54 +0200

    init

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,7 +1,26 @@
 "use client";
 
 import React from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 
 export default function MapPage() {
-    return <div className="">🚧 Page [Carte] en cours de construction 🚧</div>;
+    const centerCoordinates = [48.15, -3.25];
+    return (
+        <>
+            <div className="">🚧 Page [Carte] en cours de construction 🚧</div>
+            <div className="">
+                <MapContainer center={[48.15, -3.25]} zoom={10} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    {/* <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker> */}
+                </MapContainer>
+            </div>
+        </>
+    );
 }
